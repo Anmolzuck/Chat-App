e15422ca3c326cf8cebd2c4c2060225ba7199e62
Anmol Zakie <anmolzuck17@gmail.com>
2020-11-09 13:29:30 +0530

    Adding socket.io

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,10 @@
-io();
+const socket = io();
+
+socket.on("countUpdated", (count) => {
+  console.log("The count is updated!", count);
+});
+
+document.getElementById("increment").addEventListener("click", () => {
+  console.log("Clicked");
+  socket.emit("update");
+});
--- src/index.js
+++ src/index.js
@@ -14,9 +14,18 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
+let count = 0;
 //This event will be fired when a new socket connection is made
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New socket connection established");
+
+  socket.emit("countUpdated", count);
+
+  socket.on("update", () => {
+    count++;
+    // socket.emit("countUpdated", count);
+    io.emit("countUpdated", count); // this emitts event for all socket connections
+  });
 });
 
 server.listen(port, () => {
