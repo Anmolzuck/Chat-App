5fbc69832f2be0d298d0186d2c0e6076df1c4b94
Anmol Zakie <anmolzuck17@gmail.com>
2020-11-09 12:35:43 +0530

    Adding socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+
+//Cnnecting our server with socket.io
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+//This event will be fired when a new socket connection is made
+io.on("connection", () => {
+  console.log("New socket connection established");
+});
+
+server.listen(port, () => {
   console.log(`listening on port ${port}!`);
 });
